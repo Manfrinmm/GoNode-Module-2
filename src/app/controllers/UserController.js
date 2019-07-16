db9f6068b1f3bb1149bc57e6a18bb0db8421cec6
Manfrinmm <matheus_mm123@hotmail.com>
2019-07-16 17:47:11 -0300

    Finalização modulo 2

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,82 @@
+import * as Yup from "yup";
+
+import User from "../models/User";
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string()
+        .required()
+        .min(2)
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Falha na validação" });
+    }
+
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) return res.status(400).json({ error: "User já existente" });
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.status(201).json({
+      id,
+      name,
+      email,
+      provider
+    });
+  }
+
+  async update(req, res) {
+    //precisa melhorar as validações, quando quer somente trocar a senha
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(3),
+      password: Yup.string()
+        .min(3)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      )
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Falha na validação" });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    //alterar email
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists)
+        return res.status(400).json({ error: "User já existente" });
+    }
+
+    //alterar senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Senha inválida" });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.status(201).json({
+      id,
+      name,
+      email,
+      provider
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "GOnode@",
+  expiresIn: "7d"
+};

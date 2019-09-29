66b4a2b880afd5543f3e053baaadbe03fb0b00bc
Manfrinmm <matheus_mm123@hotmail.com>
2019-09-29 16:35:36 -0300

    Fix send e-mails

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -15,6 +15,15 @@ class Mail {
       auth: auth.user ? auth : null
     });
 
+    // verify connection configuration
+    this.transporter.verify(function(error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
     this.configureTemplates();
   }
 

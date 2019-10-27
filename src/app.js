242ac9a4ae9a00a4ce7106fe3f02b1e25ed42009
Manfrinmm <matheus_mm123@hotmail.com>
2019-10-27 11:56:56 -0300

    Add rateLimite request for API

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import { resolve } from "path";
 import Youch from "youch";
 import cors from "cors";
 import helmet from "helmet";
+import redis from "redis";
+import RateLimit from "express-rate-limit";
+import RateLimitRedis from "rate-limit-redis";
 import * as Sentry from "@sentry/node";
 import sentryConfig from "./config/sentry";
 import routes from "./routes";
@@ -32,6 +35,21 @@ class App {
       "/files",
       express.static(resolve(__dirname, "..", "tmp", "uploads"))
     );
+
+    if (process.env.NODE_ENV !== development) {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT
+            })
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100
+        })
+      );
+    }
   }
   routes() {
     this.server.use(routes);
